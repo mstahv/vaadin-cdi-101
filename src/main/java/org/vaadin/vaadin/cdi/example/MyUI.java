816a52a77f2c8d307c5fe4b27362f06941977b45
Matti Tahvonen <matti@vaadin.com>
2017-11-21 16:40:26 +0200

    Switch to CDI based deployment and make MyUI a managed bean

--- src/main/java/org/vaadin/vaadin/cdi/example/MyUI.java
+++ src/main/java/org/vaadin/vaadin/cdi/example/MyUI.java
@@ -1,11 +1,9 @@
 package org.vaadin.vaadin.cdi.example;
 
-import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
@@ -22,6 +20,7 @@ import com.vaadin.ui.VerticalLayout;
  * initialize non-component functionality.
  */
 @Theme("mytheme")
+@CDIUI("")
 public class MyUI extends UI {
 
     @Override
@@ -40,10 +39,5 @@ public class MyUI extends UI {
         layout.addComponents(name, button);
 
         setContent(layout);
-    }
-
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
     }
 }
